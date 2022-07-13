a8746ef245032d3d292ba6b00308a79b3ab8ffd6
lucas <gepeoverload@gmail.com>
2022-07-13 16:00:41 +0200

    added very basic loaders to Models

---
+++ Model/GroupLoader.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+class GroupLoader {
+
+    //Properties?
+
+    public function __construct(){
+
+        //Do we need a Constructor Here?
+
+    }
+
+    public function loadGroups(){
+
+        //Load Groups
+
+    }
+
+    public function createGroup(){
+
+        //Create Group
+
+    }
+
+    public function editGroup(){
+
+        //Edit Group
+
+    }
+
+    public function deleteGroup(){
+
+        //Delete Group
+
+    }
+
+}
---
+++ Model/StudentLoader.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+class StudentLoader {
+
+    //Properties?
+
+    public function __construct(){
+
+        //Do we need a Constructor Here?
+
+    }
+
+    public function loadStudents(){
+
+        //Load students
+
+    }
+
+    public function createStudent(){
+
+        //Create student
+
+    }
+
+    public function editStudent(){
+
+        //Edit student
+
+    }
+
+    public function deleteStudent(){
+
+        //Delete students
+
+    }
+
+}
